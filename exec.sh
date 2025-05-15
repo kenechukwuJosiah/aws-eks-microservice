1ad6adeda07932bf227b1f8743e566d68ca2d18f
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2025-05-15 21:49:10 +0100

    feat: refactor EKS cluster management script and add cluster setup functionality

--- exec.sh
+++ exec.sh
@@ -1,31 +1,27 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
-create_or_update_cluster() {
-  CONFIG_FILE="./configs/aws/eks-cluster.yml"
+COMMAND=$1
+SUBCOMMAND=$2
 
-  if [[ ! -f "$CONFIG_FILE" ]]; then
-    echo "Error: Configuration file $CONFIG_FILE does not exist."
+shift 2
+
+case "$COMMAND" in
+  cluster)
+    source  "$(dirname "$0")/scripts/cluster-setup.sh"
+    manage_cluster
+    ;;
+
+  chart)
+
+    ;;
+
+  ingress)
+    ;;
+
+  *)
+    echo "Commands: "$@""
+    echo "Usage: $0 {cluster|chart|ingress} <subcommands> [options]"
     exit 1
-  fi
+    ;;
 
-  echo "Do you want to create or update the EKS cluster? (create/update/delete)"
-
-  read -r ACTION
-
-  if [[ "$ACTION" == "create" ]]; then
-    echo "Creating EKS cluster..."
-    eksctl create cluster -f "$CONFIG_FILE"
-  elif [[ "$ACTION" == "update" ]]; then
-    echo "Updating EKS cluster..."
-    eksctl upgrade cluster -f "$CONFIG_FILE"
-  elif [[ "$ACTION" == "delete" ]]; then
-    echo "Deleting EKS cluster..."
-    eksctl delete cluster -f "$CONFIG_FILE"
-  else
-    echo "Invalid input. Please enter 'create' or 'update'."
-    exit 1
-  fi
-}
-
-create_or_update_cluster
-
+esac
---
+++ scripts/cluster-setup.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/bash
+
+manage_cluster() {
+  CONFIG_FILE="./configs/aws/eks-cluster.yml"
+
+  if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "Error: Configuration file $CONFIG_FILE does not exist."
+    exit 1
+  fi
+
+  echo "Do you want to create or update the EKS cluster? (create/update/delete)"
+
+  read -r ACTION
+
+  if [[ "$ACTION" == "create" ]]; then
+    echo "Creating EKS cluster..."
+    eksctl create cluster -f "$CONFIG_FILE"
+  elif [[ "$ACTION" == "update" ]]; then
+    echo "Updating EKS cluster..."
+    eksctl upgrade cluster -f "$CONFIG_FILE"
+  elif [[ "$ACTION" == "delete" ]]; then
+    echo "Deleting EKS cluster..."
+    eksctl delete cluster -f "$CONFIG_FILE"
+  else
+    echo "Invalid input. Please enter 'create' or 'update'."
+    exit 1
+  fi
+}
