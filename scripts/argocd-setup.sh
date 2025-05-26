aa8ed5888d5237da983ea3515e87987d7b14f2b4
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2025-05-27 00:58:54 +0100

    feat: enhance argocd setup script to check for existing namespace and remove LoadBalancer prompt

--- scripts/argocd-setup.sh
+++ scripts/argocd-setup.sh
@@ -8,24 +8,32 @@ setup_argo() {
   ENDCOLOR="\e[0m"
 
   set -e
-  kubectl create namespace argocd || { echo "Failed to create namespace"; exit 1; }
+  if ! kubectl get namespace argocd >/dev/null 2>&1; then
+    kubectl create namespace argocd || { echo "Failed to create namespace"; exit 1; }
+  else
+    echo "Namespace 'argocd' already exists. Skipping creation."
+  fi
+
+
   kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml || { echo "Failed to apply ArgoCD manifest"; exit 1; }
 
 
-  echo "Do you want to expose Argo via LoadBalancer Service Type? Enter Yes Or No"
-  read -r ANS 
+  # echo "Do you want to expose Argo via LoadBalancer Service Type? Enter Yes Or No"
+  # read -r ANS 
 
-  case "$ANS" in
-    Yes)
-      echo "Setting up LoadBalancer..."
-      kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}' || { echo -e "${RED}Failed to add LoadBalancer; Run kubectl patch svc argocd-server -n argocd -p '{\"spec\": {\"type\": \"LoadBalancer\"}}' to add LB ${ENDCOLOR}"; exit 1; }
-      ;;
-    No)
-      echo "Skipping LoadBalancer setup..."
-      ;;
-    *)
-      echo "Usage: Please Enter Yes or No"
-      exit 1
-      ;;
-  esac
+  # case "$ANS" in
+  #   Yes)
+  #     echo "Setting up LoadBalancer..."
+  #     kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}' || { echo -e "${RED}Failed to add LoadBalancer; Run kubectl patch svc argocd-server -n argocd -p '{\"spec\": {\"type\": \"LoadBalancer\"}}' to add LB ${ENDCOLOR}"; exit 1; }
+  #     ;;
+  #   No)
+  #     echo "Skipping LoadBalancer setup..."
+  #     ;;
+  #   *)
+  #     echo "Usage: Please Enter Yes or No"
+  #     exit 1
+  #     ;;
+  # esac
+
+  kubectl apply -n argocd -f ./configs/argocd || { echo "Failed to apply Ingress"; exit 1; }
 }
