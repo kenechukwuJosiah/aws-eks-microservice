79473601a7e6d2049d42f206c04cdecde1711472
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2025-05-26 15:40:47 +0100

    feat: set global API prefix and update users controller routes

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './users.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.AUTH_PORT ?? 24002);
 }
 bootstrap();
